be56128835ee16540050fe6ee719b16c1cfd1b37
Mika Lehtinen <mika.lehtinen92@gmail.com>
2020-09-01 10:32:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests, json
-import MySQLdb as mysql
-import sshtunnel
+import cv2 as cv
+import time
+import pyautogui
+from operator import itemgetter
+import random
+
 
 league = "Harvest"
 account = "Lebaa"
@@ -10,74 +14,76 @@ cfduid = "d362a63b151b7f74c6b8f9bf4efc8a31f1594966418"
 global ID
 ID = 0
 
-sshtunnel.SSH_TIMEOUT = 5.0
-sshtunnel.TUNNEL_TIMEOUT = 5.0
+inventory_x = [1295, 1355, 1405, 1455, 1505, 1555, 1605, 1660, 1725, 1775, 1825, 1875]
+inventory_y = [615, 665, 715, 770, 825]
 
-def db_command(command, variables):
-    with sshtunnel.SSHTunnelForwarder(
-        ("ssh.eu.pythonanywhere.com"),
-        ssh_username="Leba",
-        ssh_password="Lebalol123",
-        remote_bind_address=("Leba.mysql.eu.pythonanywhere-services.com", 3306),
-    ) as tunnel:
-        connection = mysql.connect(
-            user="Leba",
-            password="superpassu",
-            host="127.0.0.1",
-            port=tunnel.local_bind_port,
-            database="Leba$PoeStash",
-        )
-
-        cur = connection.cursor()
-        cur.execute(command, variables)
-        result = cur.fetchall()
-        connection.commit()
-        connection.close()
-        return result
+currencytab = []
+fragmenttab = []
+maptab = []
+ylijäämä = []
+essence = []
+delve = []
+divtab = []
 
 def parse_currency(item):
     if "Scarabs" in item['icon']:
-        print("Seo scrarabi")
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
     elif "Catalysts" in item['icon']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        ylijäämä.append(a)
     elif "Oils" in item['icon']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        currencytab.append(a)
     elif "Essence" in item['icon']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        essence.append(a)
     elif "Delve" in item['icon']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        delve.append(a)
     elif "Prophecy" in item['icon']:
-        print("ennustuksia")
+        a = [item['typeLine'], item['x'], item['y']]
+        ylijäämä.append(a)
     elif "Incubation" in item['icon']:
-        print("Inkubaattori")
+        a = [item['typeLine'], item['x'], item['y']]
+        ylijäämä.append(a)
     elif "Breach" in item['icon']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
     elif "Metamorph" in item['icon']:
-        print("Sisäelin")
+        a = [item['typeLine'], item['x'], item['y']]
+        ylijäämä.append(a)
     elif "Delirium" in item['icon']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        ylijäämä.append(a)
+    elif "Splinter" in item['icon']:
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
     else:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(value + " x " + item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        currencytab.append(a)
 
 def parse_map(item):
     if "Map" in item['typeLine']:
-        value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-        print(item['typeLine'] + " tier: " + value)
+        a = [item['typeLine'], item['x'], item['y']]
+        maptab.append(a)
     elif "Sacrifice" in item['typeLine']:
-        print(item['typeLine'])
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
     elif "Mortal" in item['typeLine']:
-        print("Mortal fragment")
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
+    elif "Splinter" in item['typeLine']:
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
+    elif "Offering" in item['typeLine']:
+        a = [item['typeLine'], item['x'], item['y']]
+        fragmenttab.append(a)
+
 
 def parse_divination(item):
-    value = item['properties'][0]['values'][0][0].strip().split('/')[0]
-    print(value + " x " + item['typeLine'])
+    a = [item['typeLine'], item['x'], item['y']]
+    divtab.append(a)
 
 def parse_armour(item):
     if len(item['sockets']) != 6:
@@ -175,15 +181,13 @@ def parse_tab(tab):
         itemit = tab['items']
         for item in itemit:
             if "Currency" in item['icon']:
-                '''parse_currency(item)'''
+                parse_currency(item)
             elif "Maps" in item['icon']:
-                print(item)
                 parse_map(item)
             elif "Map" in item['typeLine']:
                 parse_map(item)
             elif "Divination" in item['icon']:
-                '''parse_divination(item)'''
-                '''print(item)'''
+                parse_divination(item)
             elif "Armours" in item['icon']:
                 '''parse_armour(item)'''
             elif not item['identified']:
@@ -204,6 +208,8 @@ def parse_tab(tab):
                 '''parse_ring(item)'''
             elif "Quivers" in item['icon']:
                 '''parse_quiver(item)'''
+            elif "Wand" in item['icon']:
+                '''print("wandi")'''
             else:
                 print(item)
 
@@ -219,8 +225,7 @@ def request_tabs():
     resp = requests.request("GET", url, headers=headers, data=payload)
     print(resp.status_code)
     json = resp.json()
-    num = json['numTabs']
-
+    num = 1
     for x in range(num):
 
         tabiurl = url + "&tabIndex=" + str(x)
@@ -229,13 +234,88 @@ def request_tabs():
         parse_tab(tabi_json)
         tabiurl = ""
 
-def insert_account(accountName, sessionID):
-    command = "INSERT INTO poe_account (account_name, session_id) VALUES (%s, %s)"
-    db_command(command, (accountName, sessionID))
+def calc_clickpoint(x,y):
+    x_coord = 30
+    y_coord = 175
+    return [(x_coord + int(x*26.3)), (y_coord + int(y*26.3))]
 
-def insert_stash_tab(accountID,tabName):
-    command = "INSERT INTO stash_tab (account_id, tab_name) VALUES (%s, %s)"
-    db_command(command(accountID,tabName))
+def draw_crosshairs(img, coordinates):
 
+    for c in coordinates:
+        cv.drawMarker(img, (c[0], c[1]), color=(255,0,255), markerType=cv.MARKER_CROSS, markerSize=10, thickness=2)
+
+    return img
+
+def stash_to_inventory(lista):
+
+    lista.sort(key=itemgetter(2))
+    lista.sort(key=itemgetter(1))
+    pyautogui.moveTo(80,140,0.2)
+    pyautogui.leftClick()
+    pyautogui.keyDown('ctrl')
+    for l in lista:
+        coords = calc_clickpoint(l[1], l[2])
+        pyautogui.moveTo(coords[0], coords[1], random.uniform(0.15, 0.30))
+        pyautogui.leftClick()
+
+    pyautogui.keyUp('ctrl')
+
+
+def inventory_to_stash(tabi):
+    if tabi == currencytab:
+        pyautogui.leftClick(230,140,0.2)
+    if tabi == fragmenttab:
+        pyautogui.leftClick(305,140,0.2)
+    if tabi == maptab:
+        pyautogui.leftClick(170,140,0.2)
+
+    pyautogui.keyDown('ctrl')
+    for y in inventory_y:
+        for x in inventory_x:
+            pyautogui.moveTo(x,y,random.uniform(0.05,0.15))
+            pyautogui.leftClick()
+    pyautogui.keyUp('ctrl')
+
+def countdown(n):
+    print("Script starts in: ")
+    while n > 0:
+        print(str(n))
+        n = n-1
+        time.sleep(1)
+        if n == 0:
+            break
+
+markers = []
 request_tabs()
+countdown(10)
 
+while True:
+
+    stash_to_inventory(currencytab)
+    inventory_to_stash(currencytab)
+    break
+    stash_to_inventory(fragmenttab)
+    inventory_to_stash(fragmenttab)
+    stash_to_inventory(maptab)
+    inventory_to_stash(maptab)
+    stash_to_inventory(delve)
+    inventory_to_stash(delve)
+    stash_to_inventory(essence)
+    inventory_to_stash(essence)
+    stash_to_inventory(divtab)
+    inventory_to_stash(divtab)
+    #click_items(essence)
+    #click_items(divtab)
+
+    screenshot = window_capture()
+    screenshot = np.array(screenshot)
+    output = draw_crosshairs(screenshot, markers)
+    cv.imshow("Matches", output)
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv.waitKey(1) == ord("q"):
+        cv.destroyAllWindows()
+        break
+
+print("Done.")
